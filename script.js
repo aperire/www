b6823b83c312308448ac44f2a270870855403bcb
aperire <aperire0402@gmail.com>
2024-03-20 01:11:09 -0400

    init

---
+++ script.js
@@ -0,0 +1,9 @@
+function setup() {
+    mousePressed(play);
+}
+
+function mousePressed(){
+    play.click.wav;
+}
+//learned to make a click sound everytime the mouse is clicked
+// https://p5js.org/reference/#/p5.Element/mousePressed
