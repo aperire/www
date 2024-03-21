48a95037aa3144ad63c5b2315e8341fb4c77ff54
deepakkurmi5 <deepakkurmi874@gmail.com>
2024-03-21 10:21:27 +0530

    completed

--- script.js
+++
@@ -1,9 +0,0 @@
-function setup() {
-    mousePressed(play);
-}
-
-function mousePressed(){
-    play.click.wav;
-}
-//learned to make a click sound everytime the mouse is clicked
-// https://p5js.org/reference/#/p5.Element/mousePressed
